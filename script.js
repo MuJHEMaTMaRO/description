4b9124141b28eb853b34370fd9b341a6a520e3e3
MuJHEMaTMaRO <prathamsinghchouhan14@gmail.com>
2024-06-13 13:41:36 +0530

    second

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-gsap.to("#box", {
-    x:900,
-    y:200,
-    duration:2,
+var t1 = gsap.timeline()
+
+t1.from("h3",{
+    y:-20,
+    opacity:0,
+    duration:0.3,
     delay:1,
-    rotate:360, 
-    backgroundColor: "blue",
-    borderRadius:"50%"
+})
+
+t1.from("h2",{
+    y:-20,
+    opacity:0,
+    duration:0.3,
+    stagger:0.3
+})
+
+t1.from("h1",{
+    y:-20,
+    opacity:0,
+    duration:0.3,
 })
